dee0dc785ee9f748499174f4da0d479d63bc0d78
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-28 14:44:59 -0400

    Added starter code.

---
+++ notes-api/models/Note.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose")
+
+const noteSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const Note = model("Note", noteSchema)
+
+module.exports = Note
---
+++ notes-api/routes/api/noteRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router()
+const { Note } = require("../../models")
+const { authMiddleware } = require("../../utils/auth")
+
+router.use(authMiddleware)
+
+router.get("/", async (req, res) => {
+  try {
+    const notes = await Note.find({})
+    res.json(notes)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.post("/", async (req, res) => {
+  try {
+    const note = await Note.create({
+      ...req.body,
+      // The user ID needs to be added here
+    })
+    res.status(201).json(note)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    // This needs an authorization check
+    const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+
+    if (!note) {
+      return res.status(404).json({ message: "No note found with this id!" })
+    }
+
+    res.json(note)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  try {
+    // This needs an authorization check
+    const note = await Note.findByIdAndDelete(req.params.id)
+
+    if (!note) {
+      return res.status(404).json({ message: "No note found with this id!" })
+    }
+
+    res.json({ message: "Note deleted!" })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
