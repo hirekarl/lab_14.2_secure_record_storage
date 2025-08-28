d7f8a6436a322647530afc265fdeaf002992638a
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-28 15:04:04 -0400

    Added run dev script; fixed import errors in other modules.

--- notes-api/routes/api/userRoutes.js
+++ notes-api/routes/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { User } = require("../../models")
+const { User } = require("../../models/User")
 const { signToken } = require("../../utils/auth")
 
 router.post("/register", async (req, res) => {
